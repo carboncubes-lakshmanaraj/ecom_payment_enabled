4d61ebdb8af52519d3f96e40368795a60e52cfca
carboncubes-lakshmanaraj <carboncubes.lakshmanaraj@gmail.com>
2025-06-26 12:23:05 +0530

    up

---
+++ lib/stripekeys/keys.dart
@@ -0,0 +1,4 @@
+String publishableKey =
+    'pk_test_51QeezPQEnOKhuzW8GXchFGdgLIChvFuKDafA0GRlPFe1IRVsNagokvoFrU0wzDdQbigemIUX8Dc28YEafkFSOEl0006Z1OBCwY'; // <-- Replace with your key
+String secretkey =
+    'sk_test_51QeezPQEnOKhuzW8QxQiHYSSb0ZHSAv6Mjs75DMA1VlOAS7S5L3DYvZNAMmoj0A25rffX6kWLs8lSVz9E50kZ2y300NZQQvmIC';
